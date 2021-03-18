ad0fecf284ff252a3177f2ed09bbc7a528382772
shubhammehra4 <shubhammehra4@gmail.com>
2021-03-19 01:15:53 +0530

    Deploy

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,3 @@
-// const React = require("react")
-// const Layout = require("./src/components/layout")
-// const { AuthProvider } = require("./src/context/AuthProvider")
-
-// exports.wrapPageElement = ({ element, props }) => {
-//   return <Layout {...props}>{element}</Layout>
-// }
-
-// exports.wrapRootElement = ({ element }) => {
-//   return <AuthProvider>{element}</AuthProvider>
-// }
+module.exports = {
+  pathPrefix: "/reponame",
+}
