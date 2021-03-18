9b774792539a37727da624e2b6f18130bcea3eaf
shubhammehra4 <shubhammehra4@gmail.com>
2021-03-19 01:17:03 +0530

    Deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: "/tellus-private",
   siteMetadata: {
     title: `TELLUS`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
