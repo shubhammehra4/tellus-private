ffe829c0721956ff5d77b34378cea134ea1ff876
shubhammehra4 <shubhammehra4@gmail.com>
2021-03-21 15:22:27 +0530

    Breakpoints Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
@@ -8,6 +10,10 @@ module.exports = {
       },
       zIndex: {
         "-1": "-1",
+      },
+      screens: {
+        xs: "475px",
+        ...defaultTheme.screens,
       },
     },
   },
