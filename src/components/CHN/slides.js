9ccda6be91f303f90aa7229efcef3496ec71e19f
shubhammehra4 <shubhammehra4@gmail.com>
2021-03-28 18:51:47 +0530

    Meta Data IND+CHN

---
+++ src/components/CHN/slides.js
@@ -0,0 +1,70 @@
+export const slides = [
+  {
+    id: 1,
+    uid: "CHN1",
+    title: "CHINA",
+    titleColor: `#FFFFFF`,
+    description: `Endless beaches, timeless ruins, welcoming people, 
+      oodles of elephants, rolling surf, cheap prices, fun trains, 
+      famous tea and flavourful food make Sri Lanka irresistible.`,
+    descriptionColor: `white`,
+    position: `50% 50%`,
+    backgroundColor: `#BC261D`,
+  },
+  {
+    id: 2,
+    uid: "CHN2",
+    title: "BREATH-TAKING ANTIQUITY",
+    titleColor: `#DCBE9B`,
+    description: `Its modern face is dazzling, but China is no one-trick pony. The 
+    world's oldest continuous civilisation isn't all smoked glass and brushed aluminium, and 
+    while you won't be tripping over artefacts – three decades of round-the-clock development 
+    and rash town planning have taken their toll – rich seams of antiquity await.`,
+    descriptionColor: `white`,
+    position: `50% 50%`,
+    backgroundColor: `#25292C`,
+  },
+  {
+    id: 3,
+    uid: "CHN3",
+    title: "CUISINE",
+    titleColor: `#E48B5D`,
+    description: `The Chinese live to eat, expect your taste buds to be 
+    tantalised, tested and treated. Wolf down Peking duck in Beijing, 
+    melt over a Chongqing hotpot or grab a seasoned ròujiāmó (shredded pork in a bun) 
+    before climbing Hua Shan. Gobble down a steaming bowl of Lanzhou noodles, 
+    raise the temperature with some searing Hunan fare, or flag down the dim sum 
+    trolley down south. Follow your nose in China and you won't want to stop travelling.`,
+    descriptionColor: `white`,
+    position: `50% 50%`,
+    backgroundColor: `#1A2429`,
+  },
+  {
+    id: 4,
+    uid: "CHN4",
+    title: "STUPENDOUS SCENERY",
+    titleColor: `#A89580`,
+    description: `Few countries do the great outdoors like the Middle Kingdom. 
+    China's landscapes span the range from alpha to omega: take your pick from the 
+    sublime sapphire lakes of Tibet or the impassive deserts of Inner Mongolia, island-hop 
+    in Hong Kong or cycle between fairy-tale karst pinnacles around Yangshuo.`,
+    descriptionColor: `white`,
+    position: `50% 50%`,
+    backgroundColor: `#133658`,
+  },
+  {
+    id: 5,
+    uid: "CHN5",
+    title: "DIVERSITY",
+    titleColor: `#D9BB9F`,
+    description: `China is off-the-scale massive. A riveting jumble of wildly 
+    differing dialects and climatic and topographical extremes, it's like several 
+    different countries rolled into one. Take your pick from the tossed-salad ethnic 
+    mix of the southwest, the yak-butter-illuminated temple halls of Xiahe, a journey 
+    along the dusty Silk Road, spending the night at Everest Base Camp, or getting into 
+    your glad rags for a night on the Shanghai tiles. You're spoilt for choice.`,
+    descriptionColor: `white`,
+    position: `50% 50%`,
+    backgroundColor: `#393720`,
+  },
+]
---
+++ src/components/IND/slides.js
@@ -0,0 +1,55 @@
+export const slides = [
+  {
+    id: 1,
+    uid: "IND1",
+    title: "SRI LANKA",
+    titleColor: `#E07742`,
+    description: `Endless beaches, timeless ruins, welcoming people, 
+    oodles of elephants, rolling surf, cheap prices, fun trains, famous tea and flavourful food make Sri Lanka irresistible.`,
+    descriptionColor: `white`,
+    position: `50% 50%`,
+    backgroundColor: `#212725`,
+  },
+  {
+    id: 2,
+    uid: "IND2",
+    title: "THE UNDISCOVERED COUNTRY",
+    titleColor: `#FDB791`,
+    description: `You might say Sri Lanka has been hiding in plain sight. 
+    Scores of travellers have passed overhead on their way to someplace else, 
+    but years of uncertainty kept Sri Lanka off many itineraries.`,
+    descriptionColor: `white`,
+    position: `50% 50%`,
+    backgroundColor: `#162514`,
+  },
+  {
+    id: 3,
+    uid: "IND3",
+    title: "IT'S SO EASY",
+    titleColor: `#E48B5D`,
+    description: `Distances are short: see the sacred home of the world’s oldest living human-planted 
+    tree in the morning (Anuradhapura) and stand awestruck by the sight of hundreds of elephants 
+    gathering in the afternoon (Minneriya). Discover a favourite beach, meditate in a 2000-year-old 
+    temple, exchange smiles while strolling a mellow village, marvel at birds and wildflowers, try 
+    to keep count of the little dishes that come with your rice and curry. Wander past colonial gems 
+    in Colombo, then hit some epic surf.`,
+    descriptionColor: `white`,
+    position: `50% 50%`,
+    backgroundColor: `#231D19`,
+  },
+  {
+    id: 4,
+    uid: "IND4",
+    title: "RAINFORESTS & BEACHES",
+    titleColor: `#DFBEB7`,
+    description: `When you’re ready to escape the tropical climate of the 
+    coast and lowlands, head for the hills, with their temperate, achingly 
+    green charms. Verdant tea plantations and rain forested peaks beckon walkers, 
+    trekkers and those who just want to see them from a spectacular train ride. And 
+    then there are the beaches. Dazzlingly white and often untrodden, they ring the 
+    island so that no matter where you go, you’ll be near a sandy gem.`,
+    descriptionColor: `white`,
+    position: `50% 50%`,
+    backgroundColor: `#293321`,
+  },
+]
