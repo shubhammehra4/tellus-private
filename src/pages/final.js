60c37607f26072164aafd2645cf9152c432bca68
shubhammehra4 <shubhammehra4@gmail.com>
2021-04-02 13:40:29 +0530

    Email Name Modal

--- src/pages/final.js
+++ src/pages/final.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useRef } from "react"
 import FinalList from "../components/FinalList"
 import FinalSlides from "../components/finalSlides"
 import SEO from "../components/seo"
@@ -11,6 +11,34 @@ function Final() {
     activity: null,
   })
   const [showModal, setShowModal] = useState(false)
+  const email = useRef("")
+  const name = useRef("")
+
+  const validateEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return re.test(email)
+  }
+
+  const handleSubmit = e => {
+    const nameVal = name.current.value
+    const emailVal = email.current.value
+    email.current.style.borderColor = "rgba(156, 163, 175,1)"
+    if (!nameVal) {
+      name.current.focus()
+      return
+    }
+    if (!emailVal) {
+      email.current.focus()
+      return
+    }
+    if (!validateEmail(emailVal)) {
+      email.current.style.borderColor = "red"
+      email.current.focus()
+      return
+    }
+    alert(JSON.stringify({ nameVal, emailVal }))
+    setShowModal(false)
+  }
 
   return (
     <>
@@ -53,40 +81,67 @@ function Final() {
       </section>
       {showModal && (
         <section
-          id="modal"
-          className="fixed w-screen h-screen inset-0 overflow-hidden flex justify-center items-center animated fadeIn faster"
+          className="fixed z-10 w-screen h-screen inset-0 overflow-hidden flex justify-center items-center animated fadeIn faster"
           style={{ background: "rgba(0,0,0,.6)" }}
         >
-          <div className="border border-teal-500 shadow-lg modal-container bg-white w-11/12 md:max-w-md mx-auto rounded z-50 overflow-y-auto">
-            <div className="modal-content py-4 text-left px-6">
-              <div className="flex justify-between items-center pb-3">
-                <p className="text-2xl font-bold">Header</p>
-                <div className="modal-close cursor-pointer z-50">
-                  <svg
-                    className="fill-current text-black"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="18"
-                    height="18"
-                    viewBox="0 0 18 18"
-                  >
-                    <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
-                  </svg>
-                </div>
-              </div>
-              <div className="my-5">
-                <p>{JSON.stringify(state, null, 4)}</p>
-              </div>
-              <div className="flex justify-end pt-2">
-                <button
-                  onClick={() => setShowModal(false)}
-                  className="focus:outline-none modal-close px-4 bg-gray-400 p-3 text-black hover:bg-gray-300"
+          <div className="shadow-lg bg-white w-11/12 md:max-w-xl mx-auto z-50 overflow-y-auto py-4 px-6">
+            <div className="relative flex justify-center items-center pb-3 w-full">
+              <h1 className="palanquin-bold text-center text-3xl md:text-5xl text-gray-800">
+                TELL US <span className="text-gray-400">YOUR</span>
+              </h1>
+              <button
+                className="absolute top-0 right-0 z-50 focus:outline-none"
+                onClick={() => setShowModal(false)}
+              >
+                <svg
+                  className="fill-current text-black"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18"
+                  height="18"
+                  viewBox="0 0 18 18"
                 >
-                  Cancel
-                </button>
-                <button className="focus:outline-none px-4 bg-teal-500 p-3 ml-3 hover:bg-teal-400">
-                  Confirm
-                </button>
-              </div>
+                  <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+                </svg>
+              </button>
+            </div>
+            <form className="w-full my-8 md:my-10 flex flex-col justify-center items-center">
+              <label
+                htmlFor="name"
+                className="palanquin-bold text-center text-2xl md:text-3xl text-gray-800 my-4 md:my-6"
+              >
+                NAME
+              </label>
+              <input
+                id="name"
+                className="w-5/6 border-b-2 border-gray-400 outline-none text-center focus:border-blue-600"
+                ref={name}
+              />
+              <label
+                htmlFor="email"
+                className="palanquin-bold text-center text-2xl md:text-3xl text-gray-800 my-4 md:my-6"
+              >
+                EMAIL
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="w-5/6 border-b-2 border-gray-400 outline-none text-center focus:border-blue-600"
+                ref={email}
+              />
+            </form>
+            <div className="flex justify-between pt-2">
+              <button
+                className="px-4 py-3 bg-gray-600 text-white  focus:outline-none"
+                onClick={() => setShowModal(false)}
+              >
+                Close
+              </button>
+              <button
+                className="px-4 py-3 bg-indigo-600 text-white  focus:outline-none"
+                onClick={handleSubmit}
+              >
+                Confirm
+              </button>
             </div>
           </div>
         </section>
