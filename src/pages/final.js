4dd6df8e02b4083dd714b389c91d29e933525b05
Shubham Mehra <subhammehra4@gmail.com>
2021-04-24 20:01:33 +0530

    Success Message

--- src/pages/final.js
+++ src/pages/final.js
@@ -1,8 +1,12 @@
+import { AnimateSharedLayout, motion } from "framer-motion"
 import { navigate } from "gatsby-link"
 import React, { useState, useRef } from "react"
 import FinalList from "../components/FinalList"
 import FinalSlides from "../components/finalSlides"
 import SEO from "../components/seo"
+import { GrClose } from "react-icons/gr"
+import { IoCheckmarkDoneCircleOutline } from "react-icons/io5"
+import { Link } from "gatsby"
 
 function Final({ location }) {
   const [state, setState] = useState({
@@ -13,6 +17,7 @@ function Final({ location }) {
     days: null,
   })
   const [showModal, setShowModal] = useState(false)
+  const [success, setSuccess] = useState(false)
   const email = useRef("")
   const name = useRef("")
   let destination = ""
@@ -43,7 +48,8 @@ function Final({ location }) {
       return
     }
     alert(JSON.stringify({ nameVal, emailVal, ...state, destination }, null, 4))
-    setShowModal(false)
+    // setShowModal(false)
+    setSuccess(true)
   }
 
   if (!destination && typeof window !== "undefined") {
@@ -53,111 +59,142 @@ function Final({ location }) {
   return (
     <>
       <SEO title="ABOUT YOU" />
-      <section className="w-screen min-h-screen flex flex-col items-center justify-evenly mx-auto">
-        <h1 className="palanquin-bold text-center text-4xl xs:text-5xl lg:text-6xl tracking-widest text-gray-300 px-1 mt-10">
-          <span className="text-gray-800">TELLUS</span> ABOUT
-        </h1>
-        <section className="w-screen flex-grow hidden md:flex flex-col items-center justify-evenly mx-auto ">
-          <FinalList state={state} setState={setState} />
-          <button
-            disabled={
-              (state.activity &&
-                state.pace &&
-                state.personality &&
-                state.interest &&
-                state.days) === null
-            }
-            onClick={() => setShowModal(true)}
-            className="w-44 h-16 bg-gray-500 text-white lato"
-          >
-            I'M IN
-          </button>
+      <AnimateSharedLayout>
+        <section className="w-screen min-h-screen flex flex-col items-center justify-evenly mx-auto">
+          <h1 className="palanquin-bold text-center text-4xl xs:text-5xl lg:text-6xl tracking-widest text-gray-300 px-1 mt-10">
+            <span className="text-gray-800">TELLUS</span> ABOUT
+          </h1>
+          <section className="w-screen flex-grow hidden md:flex flex-col items-center justify-evenly mx-auto ">
+            <FinalList state={state} setState={setState} />
+            <button
+              disabled={
+                (state.activity &&
+                  state.pace &&
+                  state.personality &&
+                  state.interest &&
+                  state.days) === null
+              }
+              onClick={() => setShowModal(true)}
+              className="w-44 h-16 bg-gray-500 text-white lato"
+            >
+              I'M IN
+            </button>
+          </section>
+          <>
+            <FinalSlides state={state} setState={setState} />
+            <button
+              onClick={() => setShowModal(true)}
+              className={
+                (state.activity &&
+                  state.pace &&
+                  state.personality &&
+                  state.interest &&
+                  state.days) === null
+                  ? `w-32 h-12 mb-4 opacity-0 md:hidden`
+                  : `w-32 h-12 bg-gray-500 text-white lato mb-4 md:hidden`
+              }
+            >
+              I'M IN
+            </button>
+          </>
         </section>
-        <>
-          <FinalSlides state={state} setState={setState} />
-          <button
-            onClick={() => setShowModal(true)}
-            className={
-              (state.activity &&
-                state.pace &&
-                state.personality &&
-                state.interest &&
-                state.days) === null
-                ? `w-32 h-12 mb-4 opacity-0 md:hidden`
-                : `w-32 h-12 bg-gray-500 text-white lato mb-4 md:hidden`
-            }
+        {showModal && (
+          <section
+            className="fixed z-10 w-screen h-screen inset-0 overflow-hidden flex justify-center items-center animated fadeIn faster"
+            style={{ background: "rgba(0,0,0,.6)" }}
           >
-            I'M IN
-          </button>
-        </>
-      </section>
-      {showModal && (
-        <section
-          className="fixed z-10 w-screen h-screen inset-0 overflow-hidden flex justify-center items-center animated fadeIn faster"
-          style={{ background: "rgba(0,0,0,.6)" }}
-        >
-          <div className="shadow-lg bg-white w-11/12 md:max-w-xl mx-auto z-50 overflow-y-auto py-4 px-6 md:pt-14 md:pb-8">
-            <div className="relative flex justify-center items-center md:pb-3 w-full">
-              <h1 className="palanquin-bold text-center text-3xl md:text-5xl text-gray-800">
-                TELL US <span className="text-gray-400">YOUR</span>
-              </h1>
-              <button
-                className="absolute top-0 right-0 z-50 focus:outline-none"
-                onClick={() => setShowModal(false)}
+            {!success ? (
+              <motion.div
+                layoutId="box"
+                className="shadow-lg bg-white w-11/12 md:max-w-xl mx-auto z-50 overflow-y-auto pt-2 md:pb-8"
               >
-                <svg
-                  className="fill-current text-black"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="18"
-                  height="18"
-                  viewBox="0 0 18 18"
-                >
-                  <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
-                </svg>
-              </button>
-            </div>
-            <form className="w-full my-4 md:my-10 flex flex-col justify-center items-center">
-              <label
-                htmlFor="name"
-                className="palanquin-bold text-center text-2xl md:text-3xl text-gray-800 my-4 md:my-6"
+                <div className="relative flex justify-center items-center pb-4 md:pb-2 w-full">
+                  <h1 className="palanquin-bold text-center text-4xl md:text-5xl text-gray-800 pt-6 pb-4">
+                    TELL US <span className="text-gray-400">YOUR</span>
+                  </h1>
+                  <button
+                    className="absolute top-0 right-3 z-50 focus:outline-none text-gray-600 hover:text-black"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <GrClose />
+                  </button>
+                </div>
+                <form className="w-full mb-4 mt-2 md:my-8 flex flex-col justify-center items-center">
+                  <label
+                    htmlFor="name"
+                    className="palanquin-bold text-center text-2xl md:text-3xl text-gray-800 my-4 md:my-6 focus:text-indigo-800"
+                  >
+                    NAME
+                  </label>
+                  <input
+                    id="name"
+                    className="w-5/6 md:w-2/3 border-b-2 border-gray-400 outline-none text-center focus:border-blue-600"
+                    ref={name}
+                  />
+                  <label
+                    htmlFor="email"
+                    className="palanquin-bold text-center text-2xl md:text-3xl text-gray-800 my-4 md:my-6"
+                  >
+                    EMAIL
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    className="w-5/6 md:w-2/3 border-b-2 border-gray-400 outline-none text-center focus:border-blue-600"
+                    ref={email}
+                  />
+                </form>
+                <div className="flex justify-between px-6 pb-4 pt-6">
+                  <button
+                    className="h-12 w-24 bg-gray-600 text-white focus:outline-none hover:bg-gray-700"
+                    onClick={() => setShowModal(false)}
+                  >
+                    CLOSE
+                  </button>
+                  <motion.button
+                    layoutId="success"
+                    className="h-12 w-24 bg-gray-800 text-white focus:outline-none hover:bg-green-500 transition-all duration-500 ease-in-out"
+                    onClick={handleSubmit}
+                  >
+                    CONFIRM
+                  </motion.button>
+                </div>
+              </motion.div>
+            ) : (
+              <motion.div
+                layoutId="box"
+                className="shadow-lg bg-green-500 h-1/2 w-11/12 md:max-w-xl mx-auto z-50 overflow-y-auto pt-2 md:pb-8 flex"
               >
-                NAME
-              </label>
-              <input
-                id="name"
-                className="w-5/6 border-b-2 border-gray-400 outline-none text-center focus:border-blue-600"
-                ref={name}
-              />
-              <label
-                htmlFor="email"
-                className="palanquin-bold text-center text-2xl md:text-3xl text-gray-800 my-4 md:my-6"
-              >
-                EMAIL
-              </label>
-              <input
-                type="email"
-                id="email"
-                className="w-5/6 border-b-2 border-gray-400 outline-none text-center focus:border-blue-600"
-                ref={email}
-              />
-            </form>
-            <div className="flex justify-between">
-              <button
-                className="h-12 w-24 bg-gray-600 text-white  focus:outline-none"
-                onClick={() => setShowModal(false)}
-              >
-                CLOSE
-              </button>
-              <button
-                className="h-12 w-24 bg-gray-800 text-white  focus:outline-none"
-                onClick={handleSubmit}
-              >
-                CONFIRM
-              </button>
-            </div>
-          </div>
-        </section>
-      )}
+                <div className="flex flex-col justify-center items-center md:pb-2 w-full">
+                  <motion.div
+                    initial={{ opacity: 0, y: -50 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ delay: 0.5 }}
+                  >
+                    <IoCheckmarkDoneCircleOutline className="text-6xl text-white animate-bounce" />
+                  </motion.div>
+                  <motion.div
+                    layoutId="success"
+                    className="flex justify-between px-6"
+                  >
+                    <motion.h1 className="text-white text-6xl my-8">
+                      SUCCESS
+                    </motion.h1>
+                  </motion.div>
+                  <motion.p className="text-xl text-white">
+                    Please Check Your Email
+                  </motion.p>
+                  <Link to="/destination/">
+                    <motion.button className="mt-6 px-4 py-2 bg-blue-600 hover:bg-blue-700 focus:outline-none text-white">
+                      Home
+                    </motion.button>
+                  </Link>
+                </div>
+              </motion.div>
+            )}
+          </section>
+        )}
+      </AnimateSharedLayout>
     </>
   )
 }
